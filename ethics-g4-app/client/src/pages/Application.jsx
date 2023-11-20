08f2354e0f68ba122e559411b91e4e88ac9027e7
Panagiotis Karalis <pkaralis@york.citycollege.eu>
2023-11-20 13:03:13 +0200

    Update application form
    Create new pages and update the previous ones.

--- ethics-g4-app/client/src/pages/Application.jsx
+++ ethics-g4-app/client/src/pages/Application.jsx
@@ -3,14 +3,20 @@ import { Pg1 } from "../components/form-pages/Pg1";
 import { Pg2 } from "../components/form-pages/Pg2";
 import { Pg3 } from "../components/form-pages/Pg3";
 import { Pg4 } from "../components/form-pages/Pg4";
-
+import { Pg5 } from "../components/form-pages/Pg5";
+import { Pg6 } from "../components/form-pages/Pg6";
+import { Pg7 } from "../components/form-pages/Pg7";
+import { Pg8 } from "../components/form-pages/Pg8";
+import { Pg9 } from "../components/form-pages/Pg9";
+import { Pg10 } from "../components/form-pages/Pg10";
+import { Pg11 } from "../components/form-pages/Pg11";
 const Application = () => {
     const [pgNo, setPgNo] = useState(1);
 
     return (
         <div>
             <div>
-                {pgNo == 1 ? <Pg1 /> : pgNo == 2 ? <Pg2 /> : pgNo == 3 ? <Pg3 /> : <Pg4 />}
+                {pgNo == 1 ? <Pg1 /> : pgNo == 2 ? <Pg2 /> : pgNo == 3 ? <Pg3 /> : pgNo == 4 ? <Pg4 />: pgNo == 5 ? <Pg5 /> : pgNo == 6 ? <Pg6 />: pgNo == 7 ? <Pg7 /> : pgNo == 8 ? <Pg8 /> : pgNo == 9 ? <Pg9 />: pgNo == 10 ? <Pg10 /> : <Pg11 /> }
                 <center>
                     {pgNo > 1 && (
                         <button
@@ -24,7 +30,7 @@ const Application = () => {
                         Back
                         </button>
                     )}
-                    {pgNo < 4 && (
+                    {pgNo < 11 && (
                         <button
                         className="btn btn-primary mx-4"
                         type="button"
@@ -38,7 +44,7 @@ const Application = () => {
                     )}
                 </center>
                 <center>
-                    <p>Page {pgNo} / 4</p>
+                    <p>Page {pgNo} / 11</p>
                 </center>
             </div>
         </div>

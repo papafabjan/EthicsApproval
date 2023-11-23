de5d5cb3cdb6cb9764c77008a7706d3157be4843
Panagiotis Karalis <pkaralis@york.citycollege.eu>
2023-11-23 19:27:19 +0200

    Update Pg3.jsx

--- ethics-g4-app/client/src/components/form-pages/Pg3.jsx
+++ ethics-g4-app/client/src/components/form-pages/Pg3.jsx
@@ -1,8 +1,11 @@
+
 import React, { useState } from 'react';
 
 export const Pg3 = () => {
   const [showOtherInput, setShowOtherInput] = useState(false);
+  const [showOtherCheckBoxInput, setShowOtherCheckBoxInput] = useState(false);
   const [otherValue, setOtherValue] = useState('');
+  const [otherCheckBoxValue, setOtherCheckBoxValue] = useState('');
 
   const handleRadioChange = (event) => {
     if (event.target.value === 'other') {
@@ -13,6 +16,19 @@ export const Pg3 = () => {
     }
   };
 
+  const handleCheckBoxChange = (event) => {
+    if (event.target.value === 'other' && event.target.checked) {
+      setShowOtherCheckBoxInput(true);
+    } else {
+      setShowOtherCheckBoxInput(false);
+      setOtherCheckBoxValue('');
+    }
+  };
+
+  const handleOtherCheckBoxInputChange = (event) => {
+    setOtherCheckBoxValue(event.target.value);
+  };
+
   const handleOtherInputChange = (event) => {
     setOtherValue(event.target.value);
   };
@@ -20,60 +36,41 @@ export const Pg3 = () => {
   return (
     <>
       <form>
-        <div className="form-group">
+      <div className="form-group">
           <label htmlFor="ResearchProject">Title of the research project <span style={{ color: 'red' }}>*</span></label>
           <input type="text" className="form-control" id="ResearchProject" placeholder="e.g. Ethics web-site" />
         </div>
         <div className="htmlForm-group">
                 <label htmlFor="CoApplicantName">Co-Applicant Name(s)</label>    
                 <p>If there are others who will do the research with you, coma separated</p>
-                <input type="text" className="htmlForm-control" id="CoApllicantName" placeholder="e.g. Kostas Dimopoulos"/>
+                <input type="text" className="form-control" id="CoApllicantName" placeholder="e.g. Kostas Dimopoulos"/>
             </div>
             <div className="htmlForm-group">
                 <label htmlFor="CoApplicantEmail">Co-Applicant email address(es)</label>
                 <p>If there are others who will do the research with you, coma separated</p>
-                <input type="text" className="htmlForm-control" id="CoApplicantEmail" placeholder="eg. kdimopoulos@york.citycollege.eu"/>
+                <input type="text" className="form-control" id="CoApplicantEmail" placeholder="eg. kdimopoulos@york.citycollege.eu"/>
             </div>
             <div className="htmlForm-group">
                 <label htmlFor="StartDate">Proposed start date of the data collection <span style={{ color: 'red' }}>*</span></label>
-                <input type="date" className="htmlForm-control" id="StartDate"/>
+                <input type="date" className="form-control" id="StartDate"/>
             </div>
             <div className="htmlForm-group">
                 <label htmlFor="EndDate">Anticipated end date of the data collection <span style={{ color: 'red' }}>*</span></label>
-                <input type="date" className="htmlForm-control" id="EndDate" />
+                <input type="date" className="form-control" id="EndDate" />
             </div>
             <div className="htmlForm-group">
                 <label htmlFor="Fund">Is the project externally funded? <span style={{ color: 'red' }}>*</span></label>
                 <p>If <strong>YES</strong>, please specify the funding body</p>
-                <input type="text" className="htmlForm-control" id="Fund" placeholder="e.g. Ethics web-site"/>
-            </div>
-            <div className="htmlForm-group">
-                <label htmlFor="ProjectPlace">Where does the project take place? <span style={{ color: 'red' }}>*</span></label>
-                <p>Select all that apply. If the country(ies) is not listed, please add them at the end</p>
-                <input type="text" className="htmlForm-control" id="ProjectPlace" placeholder="e.g. Ethics web-site"/>
-            </div>
-        
-        <div className="form-group">
-          <label htmlFor="HealthSocialCare">Is it a health and/or social care human-interventional study?<span style={{ color: 'red' }}>*</span></label>
-          <div>
+                <li>
             <label>
-              <input type="radio" name="HealthSocialCare" value="Yes" onChange={handleRadioChange} /> Yes
+              <input type="radio" name="Fund" value="Yes" onChange={handleRadioChange} /> No Funding
             </label>
+            </li>
+            <li>
             <label>
-              <input type="radio" name="HealthSocialCare" value="No" onChange={handleRadioChange} /> No
+              <input type="radio" name="Fund" value="other" onChange={handleRadioChange} /> Other
             </label>
-          </div>
-        </div>
-
-        <div className="form-group">
-          <label htmlFor="AnotherInstitution">Is it led by an institution other than the University of York Europe Campus?<span style={{ color: 'red' }}>*</span></label>
-          <div>
-            <label>
-              <input type="radio" name="AnotherInstitution" value="Yes" onChange={handleRadioChange} /> No
-            </label>
-            <label>
-              <input type="radio" name="AnotherInstitution" value="other" onChange={handleRadioChange} /> Other
-            </label>
+            
             {showOtherInput && (
               <div>
                 <input
@@ -84,21 +81,136 @@ export const Pg3 = () => {
                 />
               </div>
             )}
+            </li>
+            </div>
+        <div className="htmlForm-group">
+          <label htmlFor="ProjectPlace">Where does the project take place? <span style={{ color: 'red' }}>*</span></label>
+          <p>Select all that apply. If the country(ies) is not listed, please add them at the end</p>
+          <ul>
+            <li>
+              <label>
+                <input type="checkbox" className="htmlForm-control" value="Bulgaria" />
+                Bulgaria
+              </label>
+            </li>
+            <li>
+                <label>
+                <input type="checkbox" className="htmlForm-control" id="ProjectPlace"/>
+                Greece
+                </label>
+                </li>
+                <li>
+                <label>
+                <input type="checkbox" className="htmlForm-control" id="Project"/>
+                Romania
+                </label>
+                </li>
+
+            <li>
+              <label>
+                <input
+                  type="checkbox"
+                  className="htmlForm-control"
+                  value="other"
+                  onChange={handleCheckBoxChange}
+                />
+                Other
+              </label>
+              {showOtherCheckBoxInput && (
+                <div>
+                  <input
+                    type="text"
+                    className="htmlForm-control"
+                    value={otherCheckBoxValue}
+                    onChange={handleOtherCheckBoxInputChange}
+                    placeholder="Please specify other countries"
+                  />
+                </div>
+              )}
+            </li>
+          </ul>
+        </div>
+        <div className="form-group">
+          <label htmlFor="HealthSocialCare">Is it a health and/or social care human-interventional study?<span style={{ color: 'red' }}>*</span></label>
+          <div>
+          <li>
+            <label>
+              <input type="radio" name="HealthSocialCare" value="Yes" onChange={handleRadioChange} /> Yes
+            </label>
+            </li>
+            <li>
+            <label>
+              <input type="radio" name="HealthSocialCare" value="No" onChange={handleRadioChange} /> No
+            </label>
+            </li>
+          </div>
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="AnotherInstitution">Is it led by an institution other than the University of York Europe Campus?<span style={{ color: 'red' }}>*</span></label>
+          <div>
+            <li>
+            <label>
+              <input type="radio" name="AnotherInstitution" value="Yes" onChange={handleRadioChange} /> No
+            </label>
+            </li>
+            <li>
+            <label>
+              <input type="radio" name="AnotherInstitution" value="other" onChange={handleRadioChange} /> Other
+            </label>
+            
+            {showOtherInput && (
+              <div>
+                <input
+                  type="text"
+                  placeholder="Please specify"
+                  value={otherValue}
+                  onChange={handleOtherInputChange}
+                />
+              </div>
+            )}
+            </li>
           </div>
         </div>
         <div className="htmlForm-group">
-                <label htmlFor="ProjectPlace">Involves human tissue?<span style={{ color: 'red' }}>*</span></label>
-                <input type="text" className="htmlForm-control" id="ProjectPlace" placeholder="e.g. Ethics web-site"/>
+                <label htmlFor="HumanTissue">Involves human tissue?<span style={{ color: 'red' }}>*</span></label>
+                <li>
+                <label>
+              <input type="radio" name="HumanTissue" value="Yes" onChange={handleRadioChange} /> Yes
+            </label>
+            </li>
+            <li>
+            <label>
+              <input type="radio" name="HumanTissue" value="No" onChange={handleRadioChange} /> No
+            </label>
+            </li>
             </div>
             <div className="htmlForm-group">
-                <label htmlFor="ProjectPlace">Clinical trial or a medical device study?<span style={{ color: 'red' }}>*</span></label>
-                <input type="text" className="htmlForm-control" id="ProjectPlace" placeholder="e.g. Ethics web-site"/>
+                <label htmlFor="ClinicalMedical">Clinical trial or a medical device study?<span style={{ color: 'red' }}>*</span></label>
+                <li>
+                <label>
+              <input type="radio" name="ClinicalMedical" value="Yes" onChange={handleRadioChange} /> Yes
+            </label>
+            </li>
+            <li>
+            <label>
+              <input type="radio" name="ClinicalMedical" value="No" onChange={handleRadioChange} /> No
+            </label>
+            </li>
             </div>
             <div className="htmlForm-group">
-                <label htmlFor="ProjectPlace">Involves social care services provided by a local authority?<span style={{ color: 'red' }}>*</span></label>
-                <input type="text" className="htmlForm-control" id="ProjectPlace" placeholder="e.g. Ethics web-site"/>
+                <label htmlFor="SocialCareServices">Involves social care services provided by a local authority?<span style={{ color: 'red' }}>*</span></label>
+                <li>
+                <label>
+              <input type="radio" name="SocialCareServices" value="Yes" onChange={handleRadioChange} /> Yes
+            </label>
+            </li>
+            <li>
+            <label>
+              <input type="radio" name="SocialCareServices" value="No" onChange={handleRadioChange} /> No
+            </label>
+            </li>
             </div>
-        
       </form>
     </>
   );
